2bf21e8372e938664c7bece14e8e47450861eb7b
gunaevart <gunaevart@mail.ru>
2018-05-05 19:55:36 +0300

    v2.01

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,7 @@ gulp.task('sass', function(){ // Создаем таск Sass
 	return gulp.src(['app/sass/**/*.sass', 'app/sass/**/*.scss']) // Берем источник
 		.pipe(sass()) // Преобразуем Sass в CSS посредством gulp-sass
 		.pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true })) // Создаем префиксы
+		.pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError)) // Преобразуем Sass в CSS посредством gulp-sass
 		.pipe(gulp.dest('app/css')) // Выгружаем результата в папку app/css
 		.pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
 });
